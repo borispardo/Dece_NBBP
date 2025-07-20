a5f59848c36facefa66aabb0d7f5942f5ffbacb6
Boris Pardo <boris.pardo8021@utc.edu.ec>
2025-07-20 18:33:50 -0500

    Configuracion del admin.py models.py y migraciones de Citas

--- Aplicaciones/Citas/models.py
+++ Aplicaciones/Citas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from Aplicaciones.CasosIntervencion.models import CasoIntervencion  
 
-# Create your models here.
+class  Cita(models.Model):
+    caso = models.ForeignKey(CasoIntervencion, on_delete=models.CASCADE)
+    fecha = models.DateTimeField()
+    profesional = models.CharField(max_length=100)
+    observaciones = models.TextField()
+
+    def __str__(self):
+        return f'Sesión - {self.fecha.strftime("%Y-%m-%d %H:%M")} ({self.profesional})'
