ffc9ad931a04e0d5fca9aee46eeabff7e9558b27
Boris Pardo <boris.pardo8021@utc.edu.ec>
2025-07-20 17:50:08 -0500

    Configuracion del admin.py models.py y migraciones

--- Aplicaciones/InformePsicopedagogico/admin.py
+++ Aplicaciones/InformePsicopedagogico/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import InformePsicopedagogico
+
+admin.site.register(InformePsicopedagogico)
 
 # Register your models here.
--- Aplicaciones/InformePsicopedagogico/models.py
+++ Aplicaciones/InformePsicopedagogico/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from Aplicaciones.Estudiantes.models import Estudiante
 
-# Create your models here.
+class InformePsicopedagogico(models.Model):
+    estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
+    fecha = models.DateField()
+    contenido = models.TextField()
+    recomendaciones = models.TextField()
+
+    def _str_(self):
+        return f'Informe de {self.estudiante.nombres} - {self.fecha}'
