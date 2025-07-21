b83b267ec3235cc01c9314a8178f623d5d5dabec
NASHELY MIKAELA BETANCOURT TOSCANO <nashely.betancourt6156@utc.edu.ec>
2025-07-20 19:02:56 -0500

    admin.py models.py y migraciones de Citas

--- Aplicaciones/Diagnosticos/models.py
+++ Aplicaciones/Diagnosticos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from Aplicaciones.Estudiantes.models import Estudiante  # Asegúrate de usar la ruta correcta
 
-# Create your models here.
+class Diagnostico(models.Model):
+    estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
+    tipo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return f'{self.tipo} - {self.estudiante.nombres} {self.estudiante.apellidos}'
+
